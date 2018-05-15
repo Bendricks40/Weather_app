cb748fc7e47531d7988d456f089fd5b020ed4d29
Bendricks40 <bendricks40@gmail.com>
2018-05-14 21:03:03 -0400

    add a onListItemClick action to display more data when a particular day of week is clicked

--- app/src/main/java/com/example/benhendricks/stormy/ui/Daily_forecast_activity.java
+++ app/src/main/java/com/example/benhendricks/stormy/ui/Daily_forecast_activity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.benhendricks.stormy.R;
 import com.example.benhendricks.stormy.adapters.DayApapter;
@@ -35,4 +37,20 @@ public class Daily_forecast_activity extends ListActivity {
 
     }
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String message;
+        String dayOfWeek = mDays[position].getDayOfTheWeek();
+        String condition = mDays[position].getSummary();
+        String highTemp = mDays[position].getTemperatureMax()+"";
+
+        message = String.format("The high on %s will be %s and it will be %s",
+                dayOfWeek,
+                highTemp,
+                condition);
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 }
